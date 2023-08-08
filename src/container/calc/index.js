257b5a2cbb927a12728db970449d3c7d0627d2ee
AlonaZhd <alona.zdakaeva@gmail.com>
2023-08-08 23:12:43 +0200

    enable number buttons, math. signs, result, and reset

---
+++ src/container/calc/index.js
@@ -0,0 +1,68 @@
+class Calc {
+  static #value = ''
+
+  static #isDot = false
+
+  static add = (newValue) => {
+    if (isNaN(this.#value[this.#value.length - 2])) {
+      if (
+        Number(this.#value[this.#value.length - 1]) === 0 &&
+        this.#isDot == false
+      ) {
+        return null
+      }
+    }
+
+    this.#value = this.#value.concat(newValue)
+    this.#output()
+    console.log(this.#value)
+  }
+
+  static #output = () => {
+    this.#save
+    window.output.innerHTML = this.#value
+  }
+
+  static dot = () => {
+    if (this.#isDot) {
+      return null
+    }
+
+    this.#value = this.#value.concat('.')
+    this.#output()
+    this.#isDot = true
+  }
+
+  static operation = (opValue) => {
+    if (isNaN(this.#value[this.#value.length - 1])) {
+      return null
+    }
+
+    this.#value = this.#value.concat(opValue)
+    this.#output()
+    console.log(this.#value)
+    this.#isDot = false
+  }
+
+  static reset = () => {
+    this.#value = ''
+    this.#isDot == false
+    this.#output()
+  }
+
+  static result = () => {
+    this.#value = String(eval(this.#value))
+    this.#output()
+    console.log(this.#value)
+  }
+
+  static #save = () => {
+    window.localStorage.setItem('calc', this.#value)
+  }
+
+  static #load = () => {
+    window.localStorage.getItem('calc') || ''
+  }
+}
+
+window.calc = Calc
